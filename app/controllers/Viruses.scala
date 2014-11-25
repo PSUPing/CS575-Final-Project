529195bc592cfc46f1c6c08c6f10e5ab02bb69ee
bwilhelm <brian.c.wilhelm@gmail.com>
2014-11-25 08:47:22 -0500

    Print returned docs to browser

--- app/controllers/Viruses.scala
+++ app/controllers/Viruses.scala
@@ -1,11 +1,8 @@
 package controllers
 
-import scala.concurrent.Future
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import org.slf4j.{LoggerFactory, Logger}
 import javax.inject.Singleton
 import play.api.mvc._
-import play.api.libs.json._
 import com.mongodb.casbah.Imports._
 
 /**
@@ -18,13 +15,6 @@ class Viruses extends Controller {
 
   private final val logger: Logger = LoggerFactory.getLogger(classOf[Viruses])
 
-  /*
-   * Get a JSONCollection (a Collection implementation that is designed to work
-   * with JsObject, Reads and Writes.)
-   * Note that the `collection` is not a `val`, but a `def`. We do _not_ store
-   * the collection reference to avoid potential problems in development with
-   * Play hot-reloading.
-   */
   def uri = MongoClientURI("mongodb://bwilhelm:CS575@ds051980.mongolab.com:51980/cs575_virus_db")
   def mongoClient =  MongoClient(uri)
   def db = mongoClient("cs575_virus_db")
@@ -34,45 +24,16 @@ class Viruses extends Controller {
   // Using case classes + Json Writes and Reads //
   // ------------------------------------------ //
 
-  import models._
-  import models.JsonFormats._
-
-//  def createVirus = Action.async(parse.json) {
-//    request =>
-//    /*
-//     * request.body is a JsValue.
-//     * There is an implicit Writes that turns this JsValue as a JsObject,
-//     * so you can call insert() with this JsValue.
-//     * (insert() takes a JsObject as parameter, or anything that can be
-//     * turned into a JsObject using a Writes.)
-//     */
-//      request.body.validate[Virus].map {
-//        virus =>
-//        // `virus` is an instance of the case class `models.Virus`
-//          collection.insert(virus).map {
-//            lastError =>
-//              logger.debug(s"Successfully inserted with LastError: $lastError")
-//              Created(s"Virus Created")
-//          }
-//      }.getOrElse(Future.successful(BadRequest("invalid json")))
-//  }
-
-  def findViruses = Action.async {
+  def findViruses = Action {
     // let's do our query
     val allDocs = collection.find()
 
-    // Print results
-    println( allDocs )
-    for(doc <- allDocs) println(doc)
-
-    // gather all the JsObjects in a list
-    val futureVirusesList: Future[List[MongoDBObject]] = Future { allDocs.toList }
-
-    // everything's ok! Let's reply with the array
-    futureVirusesList.map {
-      viruses =>
-        Ok(viruses(0))
+    var output: String = "Count: " + allDocs.size + "\n\n"
+    for(doc <- allDocs) {
+      output = output + doc + "\n\n"
     }
+
+    Ok(output)
   }
 
 }
