10b3d47ed27c93a74a624d1cbaf526c0e7205ba8
Matt Ping <matthew.ping@psualum.com>
2014-11-30 14:44:43 -0500

    Clean up of original template

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "modern-web-template"
+  val appName         = "virus-info-db"
   val appVersion      = "0.1-SNAPSHOT"
 
   val appDependencies = Seq(
